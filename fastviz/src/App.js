4c6c2283ef369498469e53ff5e95ed05fc5bbcd1
seamusgould <sgould@vassar.edu>
2022-05-20 18:10:26 -0400

    Basic functionality completed

--- fastviz/src/App.js
+++ fastviz/src/App.js
@@ -4,12 +4,13 @@
 // Analyzes the sentiment of the aggregated headlines
 // Displays the sentiment score over time as a graph
 // My api key 9a57a3e23ea54d259e5998d92b1da811
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, ResponsiveContainer } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, ResponsiveContainer , Line} from 'recharts';
 import React, {useState, useEffect, PureComponent} from 'react';
+import moment from 'moment';
+
 import './App.css';
 import Sentiment from 'sentiment';
 import axios from "axios";
-
 const sentiment = new Sentiment();
 
 function App() {
@@ -55,10 +56,20 @@ if (phrase !== "") {
     for (let i = 0; i < dates.length; i++) {
       data.push({date: dates[i], sentiment: sentiments[i], title: titles[i]});
     };
-    console.log("The data is: ", data);
+
+    data.forEach(d => {
+      d.date = moment(d.date).valueOf();
+      console.log("The parsed date is: ", d.date);
+    })
+
+
+    data.sort(function(a, b){
+      return a.date - b.date;
+    });
+    console.log("The dates is: ", data.dates);
+    console.log("The data is now: ", data);
     setData(data);
-    console.log("The data is all set");
-  };
+    };
 
   // render a scatter plot of the sentiment score over time labeled by the title of the article
 
@@ -74,18 +85,25 @@ if (phrase !== "") {
           Submit
         </button>
         <div>
-          <h5> {(Object.keys(data).length === 0) ? data : "Nothing to see here"} </h5>
           <h5> {phrase} </h5>
-          <ResponsiveContainer width="100%" height={500}>
-            <ScatterChart width={600} height={500} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-              <CartesianGrid />
-              <XAxis type="number" dataKey="dates" name="dates" />
-              <YAxis type="number" dataKey="sentiments" name="sentiments" />
-              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter name="sentiments" data={data} fill="#8884d8" />
-              <LabelList dataKey="titles" position="right" />
-            </ScatterChart>
-          </ResponsiveContainer>
+          {/* Check if data is present.  If the data is present, graph */}
+          {console.log("The data is: ", data.length)}
+          {data.length > 0 ? (
+            <ResponsiveContainer width="100%" height={500}>
+              <ScatterChart width={600} height={300} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+                <CartesianGrid />
+                {/* Set the domain to be the first and last date */}
+                <XAxis type="number" dataKey="date" name="date" domain={["auto", "auto"]} 
+                tickFormatter={(tick) => moment(tick).format("YYYY/MM/DD")}/>
+                <YAxis type="number" dataKey="sentiment" name="sentiment" unit=".0" />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter name="sentiment" data={data} fill="#8884d8" />
+                <LabelList dataKey="title" position="top" />
+              </ScatterChart>
+            </ResponsiveContainer>
+          ) : (
+              <h3>No data to display</h3>
+            )}
         </div>
       </div>
   );
